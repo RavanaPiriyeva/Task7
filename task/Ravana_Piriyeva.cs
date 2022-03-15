83a4a0cf6f931e57971a36e8eeb18bb6eecdf4de
ehtiram <ehtirammeherremov@hotmail.com>
2022-03-15 11:48:35 +0400

    first commit

---
+++ task/Ravana_Piriyeva.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //TASK1
+
+            // int[] numbers = { 5, 10, 15, 85, 18, 37, 81 };
+            //  var count = 0;
+            // for (int i = 0; i < numbers.Length; i++)
+            // {
+            //     var sum = 0;
+            //      while (numbers[i] > 0)
+            //    {
+            //          sum += numbers[i] % 10;
+            //           numbers[i] /= 10;
+            //
+            //        }
+            //       if (sum < 10)
+            //        {
+            //           count += 1;
+            //      }
+
+            //  }
+            //  Console.WriteLine("Reqemlerinin cemi 10-dan kicik olan edelerin sayi: " + count  );
+
+            //TASK2
+
+            // var count = 0;
+            // for(int i = 1; i <= 8000; i++)
+            // {
+            //     if (i % 2 == 0)
+            //     {
+            //        count += 1;
+            //     }
+            // }
+            //  Console.WriteLine(count);
+
+
+            //TASK3
+            //VARIANT1
+
+
+
+            // var number = 1;
+            // var i = 1;
+            // int result;
+            // if (number < 0)
+            // {
+            //     Console.WriteLine("duzgun eded daxil edin");
+            //
+            // }
+            // else
+            // {
+            //     while(i*i <= number)
+            //     {
+            //         i++;
+            //     }
+            //     result = i - 1;
+            //     Console.WriteLine(result);
+            // }
+
+            //TASK3
+            //VARIANT2
+
+            //   var number = 25;
+            //   int i;
+            //   int result;
+            // if (number < 0)
+            // {
+            //     Console.WriteLine("Duzgun ede daxil edin");
+            // }
+            // else { 
+            //  for( i = 1; i*i <= number; i++)
+            //   {
+            //   
+            // }
+            // result = i - 1;
+            //  Console.WriteLine(result);
+            //}
+        }
+    }
+}
